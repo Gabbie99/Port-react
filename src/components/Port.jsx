a1b883aa1434bf3d91cf4ddab8c4cf87948582be
Gabbie99 <khoinom2018.gmail.com>
2022-09-13 16:36:01 +0100

    chore:added icons

--- src/components/Port.jsx
+++ src/components/Port.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './Port.css'
+import { Phone } from 'phosphor-react'
+import {House} from 'phosphor-react'
+import {Envelope} from 'phosphor-react'
 
 function Port() {
   return (
@@ -28,8 +31,14 @@ function Port() {
             </div>
             <div className='second'>
             <div className='img-div'></div>
-                 <p>Hello, I'm</p>
-                 <h2>IDOKO <br/> CHRISTOPHER. A</h2><div className='lines'></div>
+                 <p className='my-p'>Hello, I'm</p>
+                 <h2>IDOKO <br/> CHRISTOPHER A.</h2><div className='lines'></div>
+                 <p className='my-p'>Contact Info</p>
+                 <div className='icons'>
+                 <div className='sub-icons'><Phone size={20} color="#000" weight="fill" /> 07039514066</div>
+                 <div className='sub-icons'><House size={20} color="#000" weight="fill" /> No 8 Poultry road, Jahi Abuja</div>
+                 <div className='sub-icons'><Envelope size={20} color="#000" weight="fill" /> chrixantoz@gmail.com</div>
+                 </div>
             </div>           
         </div>     
     </div>
