ec6ddec597577910151fcab8361310ff74a3b58c
Gabbie99 <khoinom2018.gmail.com>
2022-09-13 15:13:10 +0100

    feat:port work

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import './App.css';
+import Port from './components/Port';
+
+function App() {
+  return (
+    <div className="App">
+      <Port/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Port.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Port.css'
+
+function Port() {
+  return (
+    <div className='with-img'>
+        <div className='first-div'>
+            <div className='img-div'></div>
+            <div className='first'>
+                <div className='fd'>
+
+                </div>
+                <div>FASHION DESIGNER<div className='line'></div></div>
+                <div>
+                    <h5>PROFESSION</h5>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                    <h5>SKILLS</h5>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+            <div className='second'>
+                 
+            </div>           
+        </div>     
+    </div>
+  )
+}
+
+export default Port
