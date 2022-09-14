d0deaea7f7e4f0e6469f272e1b144e5a8e097bba
Gabbie99 <khoinom2018.gmail.com>
2022-09-14 13:59:15 +0100

    chore: lorem

--- src/components/Port.jsx
+++ src/components/Port.jsx
@@ -39,6 +39,15 @@ function Port() {
                  <div className='sub-icons'><House size={20} color="#000" weight="fill" /> No 8 Poultry road, Jahi Abuja</div>
                  <div className='sub-icons'><Envelope size={20} color="#000" weight="fill" /> chrixantoz@gmail.com</div>
                  </div>
+                 <div className='wrap'>
+                    <h5>PROFILE</h5>
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae omnis non exercitationem obcaecati sunt ullam cupiditate aliquam, officiis suscipit! Minima consectetur quam eius iusto quia dolores tempora cupiditate nisi. Possimus.
+                 <h5>WORK EXPERIENCE</h5>
+                 <h6>JOB TITLE HERE | COMPANY NAME</h6>
+                 <span>2019-2020</span>
+                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae corrupti quidem sunt id obcaecati aut quia dolore eius quo sit fugiat fugit delectus, ipsa dolor nemo incidunt nostrum dicta officia. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus dolorem voluptates praesentium, mollitia harum id sint atque eos, pariatur nulla, adipisci odio non odit. Omnis repellendus ex rem reiciendis officia.
+                 </div>
+                 
             </div>           
         </div>     
     </div>
