e2f4a18c8fb02492f378a5265317fafb171badb9
Gabbie99 <khoinom2018@gmail.com>
2022-10-04 15:47:32 +0100

    adding fashion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 // import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Fashion from './components/Fashion';
 import Port from './components/Port';
 
 function App() {
   return (
     <div className="App">
-      <Port/>
+      <Router>
+        <Routes>
+          <Route path='/port' element={<Port/>}/>
+          <Route path='/fashion' element={<Fashion/>}/>
+        </Routes>
+      </Router>
+      
+      
     </div>
   );
 }
---
+++ src/components/Fashion.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Fashion.css'
+
+function Fashion() {
+  return (
+    <div>
+        <nav>
+            <div>
+                Fashion Designer
+            </div>
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Testimonial</li>
+                    <li>Contact</li>
+                </ul>
+                <div>
+                    <input type="button">VISIT US</input>
+                </div>
+            </div>
+        </nav>
+
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Fashion
